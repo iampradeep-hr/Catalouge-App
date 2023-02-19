43b387c123222531fa832b20eff8c8541104221e
Pradeep <iampradeephr@gmail.com>
2023-02-20 00:26:05 +0530

    LoginPage UI Improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:catalogue_app/widgets/pages/homepage.dart';
 import 'package:catalogue_app/widgets/pages/loginpage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:catalogue_app/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      routes: {"/": (context) => LoginPage(),
-       "/home": (context) => HomePage()
-       },
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            fontFamily: GoogleFonts.lexend().fontFamily),
+        darkTheme: ThemeData.dark(),
+        routes: {
+          "/": (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute:(context) => LoginPage()
+        });
   }
 }
--- lib/widgets/pages/loginpage.dart
+++ lib/widgets/pages/loginpage.dart
@@ -1,24 +1,58 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:catalogue_app/utils/routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Container(
-            padding: EdgeInsets.all(32),
-            child: Card(
-              child: Image.asset("assets/images/login.png"),
-            )),
-        const Text(
-          "Login",
-          style: TextStyle(
-              fontSize: 30,
-              color: Colors.blueAccent,
-              fontWeight: FontWeight.bold),
-        )
-      ]),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            padding: const EdgeInsets.all(32),
+            child: Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          AnimatedTextKit(repeatForever: true, animatedTexts: [
+            TypewriterAnimatedText("Welcome",
+                textStyle:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold))
+          ]),
+
+          // const SizedBox(
+          //   height: 20,
+          // ),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Enter Username", labelText: "Username"),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Enter Password", labelText: "Password"),
+              ),
+            ]),
+          ),
+
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, MyRoutes.homeRoute);
+            },
+            style: TextButton.styleFrom(minimumSize: const Size(100, 40)),
+            child: const Text(
+              "Login",
+              style: TextStyle(fontSize: 18),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
