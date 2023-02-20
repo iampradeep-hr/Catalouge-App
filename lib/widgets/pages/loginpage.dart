c0544ff1ab4e594dd9737d880ead5470dfe7d407
Pradeep <iampradeephr@gmail.com>
2023-02-21 00:39:17 +0530

    Login screen animations impl

--- lib/widgets/pages/loginpage.dart
+++ lib/widgets/pages/loginpage.dart
@@ -4,7 +4,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,14 +48,44 @@ class LoginPage extends StatelessWidget {
             ]),
           ),
 
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, MyRoutes.homeRoute);
+          // ElevatedButton(
+          //   onPressed: () {
+          //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+          //   },
+          //   style: TextButton.styleFrom(minimumSize: const Size(100, 40)),
+          //   child: const Text(
+          //     "Login",
+          //     style: TextStyle(fontSize: 18),
+          //   ),
+          // )
+
+          InkWell(
+            onTap: () async {
+              setState(() {
+                changeButton = true;
+              });
+              await Future.delayed(Duration(seconds: 1));
+              await Navigator.of(context).pushNamed(MyRoutes.homeRoute);
+              setState(() {
+                changeButton = false;
+              });
             },
-            style: TextButton.styleFrom(minimumSize: const Size(100, 40)),
-            child: const Text(
-              "Login",
-              style: TextStyle(fontSize: 18),
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              width: changeButton ? 50 : 150,
+              height: 50,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                  color: Colors.deepPurpleAccent,
+                  borderRadius: BorderRadius.circular(changeButton ? 50 : 8)),
+              child: changeButton
+                  ? const Icon(
+                      Icons.done,
+                      color: Colors.white,
+                    )
+                  : const Text("Login",
+                      style: TextStyle(color: Colors.white, fontSize: 18)),
             ),
           )
         ]),
